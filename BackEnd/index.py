0a833b032596ba924244d87137c2e8e0dfebfa41
azure4-mac <137010606+azure4-mac@users.noreply.github.com>
2025-07-14 19:40:54 -0300

    vercel teste

--- BackEnd/index.py
+++ BackEnd/index.py
@@ -12,11 +12,16 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from forms import ProdutoForm, LoginForm, RegisterForm
 from models import db, Produto, Users
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(
     __name__,
-    static_folder="../FrontEnd/static",
-    template_folder="../FrontEnd/templates",
+    static_folder=os.path.join(BASE_DIR, "../FrontEnd/static"),
+    template_folder=os.path.join(BASE_DIR, "../FrontEnd/templates"),
 )
+
 
 app.config["SECRET_KEY"] = "sua_chave_secreta_aqui"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
